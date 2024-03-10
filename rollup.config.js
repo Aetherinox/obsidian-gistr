1b9ddfe3d2bc63a227be07292cb6bc9378e32336
Aetherinox <aetherinox@proton.me>
2024-03-10 04:36:21 -0700

    build: remove deprecated packages

--- rollup.config.js
+++ rollup.config.js
@@ -8,7 +8,7 @@
 import typescript from '@rollup/plugin-typescript';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import {terser} from "rollup-plugin-terser";
+import terser from '@rollup/plugin-terser';
 import define from 'rollup-plugin-define';
 import license from 'rollup-plugin-license'
 import {readFileSync} from 'fs';
