bd53cf6f191d972aed214962b0dda1b08544c20f
Aetherinox <aetherinox@proton.me>
2024-07-13 22:09:50 -0700

    build: bump project v1.7.0

--- gistr.js
+++ gistr.js
@@ -31,54 +31,54 @@
         - npm run env-version
 */
 
-const fs = require('fs');
-const { v5: uuid } = require('uuid');
+const fs = require('fs')
+const { v5: uuid } = require('uuid')
 
 /*
  *    declrations > package.json
  */
 
-const { version, repository } = JSON.parse(fs.readFileSync('package.json'));
-const args = process.argv.slice(2, process.argv.length);
-const action = args[0];
+const { version, repository } = JSON.parse(fs.readFileSync('package.json'))
+const args = process.argv.slice(2, process.argv.length)
+const action = args[0]
 // const a       = args[ 1 ];
 // const b       = args[ 2 ];
 
 if (action === 'guid') {
-    console.log(`${process.env.GUID}`);
+    console.log(`${process.env.GUID}`)
 } else if (action === 'setup') {
     fs.writeFileSync('.env', '', (err) => {
         if (err) {
-            console.error(err);
+            console.error(err)
         } else {
-            console.log(`Wrote to .env successfully`);
+            console.log(`Wrote to .env successfully`)
         }
-    });
+    })
 } else if (action === 'generate') {
-    const buildGuid = uuid(`${repository.url}`, uuid.URL);
-    const buildUuid = uuid(version, buildGuid);
+    const buildGuid = uuid(`${repository.url}`, uuid.URL)
+    const buildUuid = uuid(version, buildGuid)
 
     const ids = `
 VERSION=${version}
 GUID=${buildGuid}
 UUID=${buildUuid}
-`;
+`
 
-    console.log(version);
-    console.log(buildGuid);
-    console.log(buildUuid);
+    console.log(version)
+    console.log(buildGuid)
+    console.log(buildUuid)
 
     fs.writeFileSync('.env', ids, (err) => {
         if (err) {
-            console.error(`Could not write env vars: ${err}`);
+            console.error(`Could not write env vars: ${err}`)
         } else {
-            console.log(`Wrote env vars to .env`);
+            console.log(`Wrote env vars to .env`)
         }
-    });
+    })
 } else if (action === 'uuid') {
-    console.log(`${process.env.UUID}`);
+    console.log(`${process.env.UUID}`)
 } else {
-    console.log(version);
+    console.log(version)
 }
 
-process.exit(0);
+process.exit(0)
