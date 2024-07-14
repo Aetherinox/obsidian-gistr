e0748f2ed0f14938dcb719863809ee15777eff6c
Aetherinox <aetherinox@proton.me>
2024-07-14 05:07:00 -0700

    chore: add eslint / prettier

---
+++ eslint.config.js
@@ -0,0 +1,91 @@
+const tsParser = require('@typescript-eslint/parser');
+const js = require('@eslint/js');
+const globals = require('globals');
+const ts = require('@typescript-eslint/eslint-plugin');
+const eslintConfigPrettier = require('eslint-config-prettier');
+const prettier = require('eslint-plugin-prettier');
+const stylisticJs = require('@stylistic/eslint-plugin-js');
+
+module.exports = [
+{
+    files: [
+        '**/*.ts',
+        './src/**/*.ts',
+        './test/**/*.ts'
+    ],
+    plugins: {
+        '@typescript-eslint': ts,
+        'prettier': prettier,
+        '@stylistic/js': stylisticJs
+    },
+    languageOptions: {
+        parser: tsParser,
+        globals: {
+            ...globals.browser,
+        },
+        parserOptions: {
+            project: ['tsconfig.json'],
+        },
+    },
+    rules: {
+        ...js.configs.recommended.rules,
+        ...ts.configs['stylistic-type-checked'].rules,
+        // eslint/js rules
+        'indent': [1, 4],
+        'space-before-function-paren': 0,
+        'prefer-const': 1,
+        'comma-dangle': 0,
+        'keyword-spacing': ['error', { 'before': true, 'after': true }],
+        'comma-spacing': ['error', { 'before': false, 'after': true }],
+        'indent': 0,
+        'prefer-spread': 1,
+        'eqeqeq': ['error', 'smart'],
+        'no-unexpected-multiline': 0,
+        'no-prototype-builtins': 0,
+        'no-useless-escape': 1,
+        'no-mixed-operators': 1,
+        'no-control-regex': 0,
+        'no-console': 2,
+        'no-var': 2,
+        'no-undef': 0,
+        'no-redeclare': 'error',
+        'no-unused-vars': [
+            'error',
+            {
+                'argsIgnorePattern': '^_',
+                'varsIgnorePattern': '^_',
+                'ignoreRestSiblings': true
+            }
+        ],
+        '@stylistic/js/no-multi-spaces': [0, { ignoreEOLComments: true }],
+        '@stylistic/js/arrow-spacing': ['error', { 'before': true, 'after': true }],
+        '@stylistic/js/arrow-parens': ['error', 'always'],
+        '@stylistic/js/block-spacing': ['error', 'always'],
+        '@stylistic/js/brace-style': ['error', 'allman', { 'allowSingleLine': true }],
+        '@stylistic/js/comma-dangle': ['error', 'never'],
+        '@stylistic/js/comma-spacing': ['error', { 'before': false, 'after': true }],
+        '@stylistic/js/computed-property-spacing': ['error', 'always'],
+        '@stylistic/js/eol-last': ['error', 'always'],
+        '@stylistic/js/jsx-quotes': ['error', 'prefer-single'],
+        '@stylistic/js/linebreak-style': ['error', 'unix'],
+        '@stylistic/js/no-mixed-spaces-and-tabs': ['error'],
+        '@stylistic/js/no-tabs': ['error'],
+        '@stylistic/js/no-trailing-spaces': ['error', { 'skipBlankLines': true, 'ignoreComments': true }],
+        '@stylistic/js/no-whitespace-before-property': ['error'],
+        '@stylistic/js/object-curly-spacing': ['error', 'always'],
+        '@stylistic/js/quote-props': ['error', 'as-needed'],
+        '@stylistic/js/quotes': ['error', 'single', { 'allowTemplateLiterals': true }],
+        '@stylistic/js/semi': ['error', 'never'],
+        '@stylistic/js/space-in-parens': ['error', 'always'],
+        '@stylistic/js/space-infix-ops': ['error'],
+        '@stylistic/js/spaced-comment': ['error', 'always'],
+        '@stylistic/js/template-curly-spacing': ['error', 'always'],
+        '@stylistic/js/template-tag-spacing': ['error', 'always'],
+        '@stylistic/js/wrap-iife': [2, "inside", { functionPrototypeMethods: true }],
+
+        // 'prettier/prettier': ['error'],
+
+        // @typescript-eslint rules
+        '@typescript-eslint/prefer-nullish-coalescing': 'off' // require `strictNullChecks`
+    },
+}];
